acf05491ffdf371737493ac541c6c00dfbd2282a
Jon Maur <jonmaur@gmail.com>
2014-11-25 19:38:39 -0500

    Added a DebugServer sample, based off the Finite State Machine sample.

---
+++ samples/7-debugserver/main/main.lua
@@ -0,0 +1,57 @@
+-- makes output work better on most hosts, or when running through Sublime Text.
+io.stdout:setvbuf("no")
+
+local Draggin = require "draggin/draggin"
+local Display = require "draggin/display"
+local DebugServer = require "draggin/debugserver"
+
+----------------------------------------------------------------
+-- App Title
+Draggin.appTitle = "Ninja Finite State Machine"
+
+----------------------------------------------------------------
+-- Display
+-- params: <string> window text, <number> virtual width, <number> virtual height, <number> screen width, <number> screen height
+Display:init(Draggin.appTitle, 1920/2, 1080/2, 1920/2, 1080/2, false)
+
+----------------------------------------------------------------
+-- AUDIO
+if MOAIUntzSystem then
+	MOAIUntzSystem.initialize(44100, 1000)
+	print("Untz audio initialized")
+end
+
+local GameStateManager = require "draggin/gamestatemanager"
+local NinjaState = require "ninjastate"
+local ninjastate = NinjaState.new()
+
+
+-- some global functions we'll be calling from the debug server
+-- currently the debug connection can only execute code in the global space
+local debugserver = DebugServer.new()
+
+function jump()
+	ninjastate.actions:injectAction("jump")
+end
+
+function attack()
+	ninjastate.actions:injectAction("attack")
+end
+
+function quit()
+	ninjastate.actions:injectAction("quit")
+end
+
+-- Override the print function and send it all back to any clients
+local realPrint = print
+print = function (...)
+	realPrint(...)
+	local msg = ""
+	local args = { ... }
+	for k, v in pairs(args) do
+		msg = msg..tostring(v)
+	end
+	debugserver:send(msg.."\r\n")
+end
+
+GameStateManager.pushState(ninjastate)
---
+++ samples/7-debugserver/main/ninjastate.lua
@@ -0,0 +1,119 @@
+local Draggin = require "draggin/draggin"
+local Display = require "draggin/display"
+local Sprite = require "draggin/sprite"
+local ActionMap = require "draggin/actionmap"
+local TextBox = require "draggin/textbox"
+local Ninja = require "ninja"
+
+local viewport = Display.viewport
+local virtualWidth = Display.virtualWidth
+local virtualHeight = Display.virtualHeight
+
+local ninjastate = {}
+
+function ninjastate.new()
+
+	local state = {}
+
+	state.name = "NinjaState"
+
+	local layers = {}
+	local mainlayer
+
+	local header
+	local txt
+	local ninja
+	local ninjaThread
+
+	local actions
+
+
+
+	function state:init()
+
+		-- create the main layer
+		mainlayer = MOAILayer2D.new()
+		mainlayer:setViewport(viewport)
+		layers[#layers+1] = mainlayer
+
+		header = TextBox.new("LiberationSansNarrow-Regular", 24)
+		header:setColor(1, 1, 1, 1)
+		header:setRect(virtualWidth/16, virtualHeight/16, virtualWidth - virtualWidth/16, virtualHeight/4)
+		header:setAlignment(MOAITextBox.CENTER_JUSTIFY, MOAITextBox.CENTER_JUSTIFY)
+		header:setString('telnet to debug server on port 12345\nyou can send  jump() attack() quit()')
+
+		txt = TextBox.new("LiberationSansNarrow-Regular", 24)
+		txt:setColor(1, 1, 1, 1)
+		txt:setRect(virtualWidth/16, virtualHeight - virtualHeight/4, virtualWidth - virtualWidth/16, virtualHeight - virtualHeight/16)
+		txt:setAlignment(MOAITextBox.CENTER_JUSTIFY, MOAITextBox.CENTER_JUSTIFY)
+		txt:setString('[space] jump, [enter] attack, [esc] quit')
+
+		ninja = Ninja.new()
+
+
+		-- action map
+		actions = ActionMap.new()
+		-- this will expose the action map to others to make it easier to inject actions
+		state.actions = actions
+
+		actions:addAction("quit", string.char(27))
+		actions:addAction("jump", ' ', nil)
+		actions:addAction("attack", 'b', nil)
+		actions:addAction("attack", string.char(13), nil)
+
+		print(state.name.." init() done.")
+	end
+
+	function state:gotFocus()
+
+		print(state.name.." got focus")
+
+		header:insertIntoLayer(mainlayer)
+		txt:insertIntoLayer(mainlayer)
+
+		-- the ninja
+		ninja:setLoc(virtualWidth/2, virtualHeight/2)
+		mainlayer:insertProp(ninja)
+
+		-- Make this state's layers the current render table
+		-- Only one render table can be actively rendered
+		MOAIRenderMgr.setRenderTable(layers)
+
+		local function ninjaFunc()
+			while true do
+
+				if actions:stateOf("quit") == "down" then
+					os.exit()
+
+				elseif actions:stateOf("jump") == "down" then
+					ninja:tryJumpState()
+
+				elseif actions:stateOf("attack") == "down" then
+					ninja:tryAttackState()
+				end
+
+				ninja:update()
+				actions:update()
+				coroutine.yield()
+			end
+		end
+
+		ninjaThread = MOAIThread.new()
+		ninjaThread:run(ninjaFunc)
+	end
+
+	function state:lostFocus()
+		print(state.name.." lost focus")
+		mainlayer:removeProp(ninja)
+		header:removeFromLayer()
+		txt:removeFromLayer()
+
+		ninjaThread:stop()
+		ninjaThread = nil
+	end
+
+
+	return state
+end
+
+return ninjastate
