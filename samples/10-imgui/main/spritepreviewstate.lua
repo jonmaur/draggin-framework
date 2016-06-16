a3af5689b89c5ba43a9d0aa334796f2c2ee172a7
Jon Maur <jonmaur@gmail.com>
2016-06-16 09:53:53 -0400

    Added more ImGui functions to the sample

--- samples/10-imgui/main/spritepreviewstate.lua
+++ samples/10-imgui/main/spritepreviewstate.lua
@@ -65,6 +65,8 @@ function spritepreviewstate.new()
 			local v = MOAIImVec2.new()
 			local red = MOAIImVec4.new()
 			red:set(1, 0, 0, 1)
+			local checked = false
+			local activebutton = 1
 			
 			while true do
 				-- imgui test
@@ -74,8 +76,33 @@ function spritepreviewstate.new()
 					MOAIImGui.BeginChild(1, v)
 						MOAIImGui.TextColored(red, "Animation: ")
 						MOAIImGui.Text(tostring(ninja:getCurrentAnimationName()))
+						MOAIImGui.TextDisabled("Disabled text. No point in using format strings here, construct the string in lua.")
 					MOAIImGui.EndChild()
+					MOAIImGui.TextWrapped("Wrapped text. No point in using format strings here, construct the string in lua.")
+					MOAIImGui.TextUnformatted("Text with NULL (\0) characters. NULL's show up as \0, but that's ok.")
 					MOAIImGui.Text("Animation: "..tostring(ninja:getCurrentAnimationName()))
+					MOAIImGui.LabelText("Animation: ", tostring(ninja:getCurrentAnimationName()))
+					MOAIImGui.Bullet()
+					MOAIImGui.BulletText("Bullet Text")
+					if MOAIImGui.Button("Press test") then
+						print("button pressed")
+					end
+					if MOAIImGui.InvisibleButton("invisible id", v) then
+						print("invisible button pressed")
+					end
+					if MOAIImGui.SmallButton("Small Press test") then
+						print("small button pressed")
+					end
+					
+					checked = MOAIImGui.Checkbox("Checkbox", checked)
+					if checked then
+						MOAIImGui.Text("Checked! ")
+					end
+					activebutton = MOAIImGui.RadioButton("Radio 1", activebutton, 1)
+					activebutton = MOAIImGui.RadioButton("Radio 2", activebutton, 2)
+					activebutton = MOAIImGui.RadioButton("Radio 3", activebutton, 3)
+					activebutton = MOAIImGui.RadioButton("Radio 4", activebutton, 4)
+					MOAIImGui.Text("active! "..tostring(activebutton))
 				MOAIImGui.End()
 
 				coroutine.yield()
